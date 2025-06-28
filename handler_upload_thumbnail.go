c6a1fcba4a00e0d5063515520ca8ea94d272a22d
trolioSFG <sergueit@gmail.com>
2025-06-28 12:57:33 +0200

    CH1L7 Using filesystem

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -8,7 +8,10 @@ import (
 	"github.com/google/uuid"
 
 	"io"
-	"encoding/base64"
+//	"encoding/base64"
+	"mime"
+	"os"
+	"path/filepath"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -47,11 +50,15 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 
 	media := header.Header.Get("Content-Type")
 	fmt.Println("Content-Type:", media)
+
+	/**
+	Read later when copying to disk
 	data, err := io.ReadAll(file)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Could not read file", err)
 		return
 	}
+	**/
 
 	video, err := cfg.db.GetVideo(videoID)
 	if err != nil {
@@ -74,9 +81,38 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 	thumbURL := "http://localhost:8091/api/thumbnails/" + videoID.String()
 	video.ThumbnailURL = &thumbURL 
 	***/
+
+	/**
 	encodedData := base64.StdEncoding.EncodeToString(data)
 	dataURL := fmt.Sprintf("data:%s;base64,%s", media, encodedData)
+	***/
+	extensions, err := mime.ExtensionsByType(media)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "MIME Type", err)
+		return
+	}
 
+	// The returned extensions will each begin with a leading dot, as in ".html"
+	ext := extensions[0]
+	fileName := videoID.String() + ext
+	fpath := filepath.Join(cfg.assetsRoot, fileName)
+	dstFile, err := os.Create(fpath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Create file", err)
+		return
+	}
+
+	written, err := io.Copy(dstFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Copy", err)
+		return
+	}
+
+	fmt.Println(written, "bytes copied to file:", fpath)
+	file.Close()
+
+
+	dataURL := "http://localhost:8091/assets/" + fileName
 	video.ThumbnailURL = &dataURL
 
 	err = cfg.db.UpdateVideo(video)
